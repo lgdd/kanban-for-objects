a34c71a19c7ab8e50cd53bfad67ce980bc87b843
lgdd <louis-guillaume.durand@liferay.com>
2024-03-27 09:58:58 +0100

    chore: refactor card styles

--- client-extensions/kanban-objects/src/components/State.js
+++ client-extensions/kanban-objects/src/components/State.js
@@ -6,24 +6,24 @@ import Object from "./Object";
 const State = ({ state, objects, objectDefinition, numberOfStates }) => {
   return (
     <div key={state.id} className="card" style={{ minWidth: `${100 / numberOfStates}%` }}>
-      <div className="card-body">
+      <div class="card-header">
         <h2 className="card-title" >{camelCaseToWords(state.key)}</h2>
-        <Droppable droppableId={state.key}>
-          {(provided, snapshot) => (
-            <div ref={provided.innerRef} {...provided.droppableProps}
-              className={"droppable-container" + (snapshot.isDraggingOver ? " dragging-over" : "")}>
-              {objects && objects.filter((object) => { return object.state.key === state.key }).map((object, index) => {
-                return <Object
-                  key={index}
-                  index={index}
-                  object={object}
-                  definition={objectDefinition} />
-              })}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
       </div>
+      <Droppable droppableId={state.key}>
+        {(provided, snapshot) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}
+            className={"card-body droppable-container" + (snapshot.isDraggingOver ? " dragging-over" : "")}>
+            {objects && objects.filter((object) => { return object.state.key === state.key }).map((object, index) => {
+              return <Object
+                key={index}
+                index={index}
+                object={object}
+                definition={objectDefinition} />
+            })}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
     </div>
   )
 }
--- client-extensions/kanban-objects/src/services/utils.js
+++ client-extensions/kanban-objects/src/services/utils.js
@@ -13,4 +13,12 @@ const camelCaseToWords = (str) => {
   }).join(' ');
 }
 
-export { objectIsEmpty, objectIsNotEmpty, camelCaseToWords };
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+export { objectIsEmpty, objectIsNotEmpty, camelCaseToWords, reorder };
